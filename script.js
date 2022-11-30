e0dd151d798a9ad8f55c67919b16e4b9e5dab544
unknown <shannoncoleman16@gmail.com>
2022-11-30 00:42:14 -0500

    added prompt questions

--- script.js
+++ script.js
@@ -5,14 +5,18 @@ function generatePassword() {
   console.log ("Hey you did it")
  
   // User will be prompted for password criteria
-  
-  //User asked for character types to include in password
+  var Uppercase = confirm("Would you like to use uppercase letters?");
+  var Lowercase = confirm("Would you like to use lowercase letters?");
+  var numbers = confirm("Would you like to include numbers?");
+  var specialChar = confirm("Would you like to include special characters?");
+  var characterLength = confirm("How long would you like the password to be?");
 
-  var specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-  var numbers = "0123456789";
+  //User asked for character types to include in password
   var letters = "abcdefghijklmnop";
   var Uppercase = letters.toUpperCase();
   var Lowercase = letters.toLowerCase();
+  var numbers = "0123456789";
+  var specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
   var characterLength = 128;
   
 for (var i = 0; i <= 128; i++) {
@@ -22,7 +26,9 @@ for (var i = 0; i <= 128; i++) {
 
 
 //User prompted for length of password
-
+function prompts() {
+  characterLength = prompt ("How many characters would you like your password to include?");
+}
 
 
 
