ddf9ac28a94b9dc320b33d1e6f366bcf9d76802f
unknown <shannoncoleman16@gmail.com>
2022-11-29 23:51:22 -0500

    added comment

--- script.js
+++ script.js
@@ -2,33 +2,17 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  // YOUR CODE GOES HERE
-var isUppercase = confirm('Please use at least one uppercase letter.');
-var isLowercase = confirm('Please use at least one lowercase letter.');
-var isSpecial = confirm('Please use at least one special character.');
-var isNumber = confirm('Please use at least one number.');
+  console.log ("Hey you did it")
+ 
+  // User will choose the be asked for password
 
-if (isUppercase) {
-  // Use Uppercase letters
-  console.log ('Uppercase')
-}
 
-if (isLowercase) {
-  // Use Lowercase letters
-  console.log ('lowercase')
-}
 
-if (isSpecial) {
-  // Use Special characters
-  console.log ('special')
-}
 
-if (isNumber) {
-  // Use Numbers
-  console.log ('numbers')
-}
 
-return ""
+
+
+  return "password";
 }
 
 // Write password to the #password input
