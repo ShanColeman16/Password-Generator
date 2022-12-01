78e492911145d5edb085a33fc2cafe793972af8f
unknown <shannoncoleman16@gmail.com>
2022-11-30 21:56:26 -0500

    added array

--- script.js
+++ script.js
@@ -8,61 +8,49 @@ function generatePassword() {
   var Uppercase = letters.toUpperCase();
   var characterLength = 0;
   var chooseFrom = "";
-
   // YOUR CODE GOES HERE
-  var characterLength = prompt("How many characters would you like to include in your password?" );
+
+  var characterLength = prompt("How many characters would you like to include in your password?");
   var isUppercase = confirm("Would you like to use uppercase?");
   var isLowercase = confirm("Would you like to use uppercase?");
   var isSpecial = confirm("Would you like to use a special character?");
   var isNumber = confirm("Would you like to use number?");
-  var computerChoice = Math.floor(Math.random() * characterLength);
+  var random = Math.floor(Math.random() * characterLength);
 
+    //Array
+    var collection = ['Uppercase', 'Lowercase', 'Special', 'Number']
+    chooseFrom += special (Math.floor(Math.random() * characterLength));
 
   if (isUppercase) {
     // Use Uppercase letters
     console.log('Uppercase')
-    chooseFrom += Uppercase;
   }
 
   if (isLowercase) {
     // Use Lowercase letters
     console.log('lowercase')
-    chooseFrom += Lowercase;
   }
 
-  if (isSpecial) {
+  if (special) {
     // Use Special characters
     console.log('special')
-    chooseFrom += special;
   }
-
 
   if (isNumber) {
-     // Use Numbers
-     console.log('numbers')
-     chooseFrom += numbers;
-    
+    // Use Numbers
+    console.log('numbers')
+const numbers = Number(window.prompt("Type a number", ""));
   }
-if (characterLength < 8) {
-  prompt("Password needs to be at least 8 characters long");
-  
-} else if (passwordlength > 128); {
-  prompt("Password needs to be no longer than 128 characters");
+
+
+
+
+
+
+
+  return randompassword
+
 }
-
-
-  }
-
-  chooseFrom += Uppercase;
-  chooseFrom += Lowercase;
-  chooseFrom += special;
-  chooseFrom += numbers;
-
-  //Grab Ramdon Letters
-  console.log (chooseFrom);
-
-  
-
 
 // Write password to the #password input
 //do not have to do anything here
@@ -72,9 +60,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-  return ""
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
